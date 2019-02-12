23680f991433846b0fba17626c102163104db1a6
Rajput-Dell\Rajput <abhishekrajput676@gmail.com>
2019-02-12 18:39:23 +0530

    Initial commit

---
+++ src/java/FoodSpots/Index.java
@@ -0,0 +1,50 @@
+package FoodSpots;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Index extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            RequestDispatcher rd = request.getRequestDispatcher("headlinks.html");
+            rd.include(request, response);
+
+            out.println("<body>");
+            rd = request.getRequestDispatcher("header.html");
+            rd.include(request, response);
+
+            rd = request.getRequestDispatcher("index.html");
+            rd.include(request, response);
+            
+            rd = request.getRequestDispatcher("footer.html");
+            rd.include(request, response);
+
+            rd = request.getRequestDispatcher("footlinks.html");
+            rd.include(request, response);
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
